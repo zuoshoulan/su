4f0f19bb7769e0cebe49d09859040388675f6e1e
lwk <79523601@qq.com>
2022-11-02 14:47:15 +0800

    insertsort

--- algorithm/src/main/java/com/su/algo/InsertSort.java
+++ algorithm/src/main/java/com/su/algo/InsertSort.java
@@ -5,4 +5,21 @@ package com.su.algo;
  * @Date 2022/8/3 16:48
  */
 public class InsertSort {
+
+    public static int[] arr = {5, 4, 3, 1, 2, 9, 8, 6, 7, 0};
+
+    public static void main(String[] args) {
+        Utils.printArr(arr);
+        System.out.println("---");
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = i; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    Utils.swap(arr, j, j - 1);
+                    Utils.printArr(arr);
+                }
+            }
+        }
+        System.out.println("---");
+        Utils.printArr(arr);
+    }
 }
