ecae82f6fa376fa8f0ee5c4c58bba3e2a46f7234
lwk <79523601@qq.com>
2022-11-03 12:31:43 +0800

    chapter4

---
+++ algorithm/src/main/java/com/su/algo/ShellSort.java
@@ -0,0 +1,17 @@
+package com.su.algo;
+
+public class ShellSort implements ArrSortInterface {
+
+    public static int[] arr = {5, 4, 3, 1, 2, 9, 8, 6, 7, 0};
+
+    public static void main(String[] args) {
+
+
+    }
+
+    @Override
+    public int[] sort(int[] arr) {
+        //todo
+        return arr;
+    }
+}
