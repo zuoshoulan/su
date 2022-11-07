2e4ed2414ee6707635add0a26eec029280ed9a99
lwk <79523601@qq.com>
2022-11-07 15:09:59 +0800

    斐波那契数列 递归算法 循环算法

---
+++ algorithm/src/main/java/com/su/algo/FibonacciSeq.java
@@ -0,0 +1,47 @@
+package com.su.algo;
+
+/**
+ * 斐波那契数列
+ */
+public class FibonacciSeq {
+
+    //递归算法
+    public static int getFibonacciRecursion(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        return getFibonacciRecursion(n - 1) + getFibonacciRecursion(n - 2);
+    }
+
+    //循环算法
+    public static int getFibonacci(int index) {
+        if (index == 0) {
+            return 1;
+        }
+        if (index == 1) {
+            return 1;
+        }
+        int[] memorandum = new int[index + 1];
+        memorandum[0] = 1;
+        memorandum[1] = 1;
+        for (int i = 2; i <= index; i++) {
+            memorandum[i] = memorandum[i - 1] + memorandum[i - 2];
+
+        }
+        return memorandum[index];
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            int n = i;
+            int seq = getFibonacciRecursion(n);
+            System.out.println("Recursion n=" + n + " ,seq=" + seq);
+            int seq1 = getFibonacci(n);
+            System.out.println("seq n=" + n + " ,seq=" + seq1);
+        }
+
+    }
+}
